c4aca3578ca6c398e8246f55a270889f76c158c0
René Wernicke <r.wernicke@prosoz.de>
2020-05-08 00:04:22 +0200

    Get episode titles from full episodes

--- crawl.rb
+++ crawl.rb
@@ -3,17 +3,25 @@ require 'nokogiri'
 
 class CrawPatrol
 
-    def crawl_for_new
+    def get_episode_titles
         response = Faraday.get 'http://www.nickjr.de/paw-patrol/videos/'
         doc = Nokogiri::HTML(response.body)
-        class_name = "tile-episode-flag"
+        class_name = 
         
-        episode_markers = doc.xpath("//*[contains(concat(' ', normalize-space(@class), ' '), ' #{class_name} ')]")
+        stream_items = all_items_with_class(doc, "stream-block-item")
 
-        puts episode_markers
+        episodes = stream_items.reject { |item| item.xpath('./a/div/div/div[@class="stream-label stream-tile-flag tile-episode-flag"]').empty?}
+
+        episodes.map { |episode| episode.xpath('./div/b[@class="tooltip-title"]').inner_text}
+    end
+
+    private
+
+    def all_items_with_class(parent_node, class_name)
+        parent_node.xpath("//div[contains(concat(' ', normalize-space(@class), ' '), ' #{class_name} ')]")
     end
 end
 
 patrol = CrawPatrol.new
 
-patrol.crawl_for_new
+puts patrol.get_episode_titles
